c55784da01fdf65c0b32e29f00232facf4369b47
Guilherme Araujo <guilherme.araujo1535@gmail.com>
2023-08-20 05:17:12 -0300

    instalando biblioteca react-router-dom e criando a primeira rota "/"

---
+++ src/pages/Public/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Loading from '../../../components/common/Loading/Loading';
+
+const HomePage = () => {
+    return (
+        <><div>
+            <h1>Welcome to Our Website</h1>
+            <p>This is the homepage of our awesome website.</p>
+        </div>
+            <Loading /></>
+    );
+};
+
+export default HomePage;
