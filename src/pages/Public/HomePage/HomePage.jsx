b66a2d9a195f3da2864f81050fdb39badb8fcf03
Guilherme Araujo <guilherme.araujo1535@gmail.com>
2023-08-20 06:40:09 -0300

    Adicionado cabeçalho e main na pagina Home

--- src/pages/Public/HomePage/HomePage.jsx
+++ src/pages/Public/HomePage/HomePage.jsx
@@ -1,14 +1,44 @@
 
 import React from 'react';
-import Loading from '../../../components/common/Loading/Loading';
+import Container from '../../../components/common/Container/Container';
+import Header from '../../../components/public/Header/Header';
+import Col from '../../../components/common/Col/Col';
+import Row from '../../../components/common/Row/Row';
+import './HomePage.css';
+import Padding from '../../../components/common/Padding/Padding';
+import Button from '../../../components/common/Button/Button';
+import Title from '../../../components/common/Title/Title';
 
 const HomePage = () => {
     return (
-        <><div>
-            <h1>Welcome to Our Website</h1>
-            <p>This is the homepage of our awesome website.</p>
-        </div>
-            <Loading /></>
+        <>
+            <Container type='xl'>
+                <Header></Header>
+            </Container>
+            <main class="main">
+                <Row>
+                    <Col md={6} col={12}>
+                        <div class="main-col-text">
+                            <Padding pl={5}>
+                                <Title type={'h1'}>
+                                    Seguro Pesados da Porto
+                                </Title>
+                                <p>
+                                    A proteção que acompanha você em qualquer lugar e em
+                                    qualquer situação.
+                                </p>
+                                <Button class={'btn-primary'}>
+                                    Solicite uma proposta
+                                </Button>
+                            </Padding>
+                        </div>
+                    </Col >
+                    <Col md={6} col={12}>
+                        <div className="main-img"></div>
+                    </Col>
+                </Row >
+            </main >
+        </>
     );
 };
 
